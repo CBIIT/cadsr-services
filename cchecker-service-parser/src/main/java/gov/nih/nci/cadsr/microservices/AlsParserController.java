c7c784bfdd6ce2cb7810177cc8f48e79cd8c6c6b
asafieva <natalia.asafieva@nih.gov>
2018-07-11 15:23:14 -0400

    AlsParserController changes

--- cchecker-service-parser/src/main/java/gov/nih/nci/cadsr/microservices/AlsParserController.java
+++ cchecker-service-parser/src/main/java/gov/nih/nci/cadsr/microservices/AlsParserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gov.nih.nci.cadsr.data.ALSData;
+import gov.nih.nci.cadsr.data.ALSError;
 import gov.nih.nci.cadsr.data.CCCError;
 import gov.nih.nci.cadsr.parser.Parser;
 import gov.nih.nci.cadsr.parser.impl.AlsParser;
@@ -39,7 +40,7 @@ public class AlsParserController {
 		String filePath = buildFilePath(fileName);
 		logger.debug("Parse file in path: " + filePath);
 		try {
-			ALSData alsData = alsParser.parse(filePath);
+			ALSData alsData = alsParser.parse(filePath, fileName);
 			HttpHeaders httpHeaders = new HttpHeaders();
 			httpHeaders.add("Content-Type", "application/json");	
 
@@ -62,7 +63,10 @@ public class AlsParserController {
 		ALSData error = new ALSData();
 		CCCError cccError = new CCCError();
 		String noFilePathError = "Server problems Parser component no file information received";
-		cccError.addError(noFilePathError);
+		ALSError alsError = new ALSError();
+		alsError.setErrorDesc(noFilePathError);
+		alsError.setErrorSeverity("FATAL");
+		cccError.addAlsError(alsError);
 		error.setCccError(cccError);
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add("Content-Type", "application/json");
