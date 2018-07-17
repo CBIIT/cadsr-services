da25e3168155b8d1c1a80ea2fc443a43400a5fc1
Einolf <einolfs@nci-02090032-ml.nih.gov>
2018-07-17 16:15:13 -0400

    removed exposing port 4801

--- cchecker-docker/build.sh
+++ cchecker-docker/build.sh
@@ -2,4 +2,4 @@
 docker build -t cadsr-services .
 docker kill cadsr-services
 docker rm cadsr-services
-docker run --name cadsr-services -p 8080:8080 -p 4801:4801 cadsr-services
+docker run --name cadsr-services -p 8080:8080  cadsr-services
