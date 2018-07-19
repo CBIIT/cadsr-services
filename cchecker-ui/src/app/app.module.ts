f4de91395eb13d72cfbc12e3da600ed9739ba58d
regulator95 <seinolf@gmail.com>
2018-07-19 18:13:03 -0400

    added navigation to template

--- cchecker-ui/src/app/app.module.ts
+++ cchecker-ui/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes,  RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { AlsFormComponent } from './als-form/als-form.component';
 import { AlsFormListComponent } from './als-form-list/als-form-list.component';
 import { AlsUploadFormComponent } from './als-upload-form/als-upload-form.component';
 import { AlsReportComponent } from './als-report/als-report.component';
 import { NavigationComponent } from './navigation/navigation.component';
+
+const APP_ROUTES: Routes = [
+  // Default empty path
+  { path: 'forms', component: AlsFormListComponent },
+  { path: 'report', component: AlsReportComponent },
+  { path: '', component: AlsUploadFormComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { NavigationComponent } from './navigation/navigation.component';
     NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
