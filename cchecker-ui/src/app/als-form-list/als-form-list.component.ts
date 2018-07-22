eeff1925a26c28532f74f01ec1fab101e50e0f1f
regulator95 <seinolf@gmail.com>
2018-07-22 12:13:38 -0400

    renamed functions in form list ts file

--- cchecker-ui/src/app/als-form-list/als-form-list.component.ts
+++ cchecker-ui/src/app/als-form-list/als-form-list.component.ts
@@ -13,17 +13,16 @@ export class AlsFormListComponent implements OnInit {
     // test data //
     this.data = {"formsList":[{"isValid":false,"errors":[],"formName":"Enrollment","questionsCount":31},{"isValid":true,"errors":[],"formName":"Histology and Disease","questionsCount":10},{"isValid":false,"errors":[],"formName":"Administrative Enrollment","questionsCount":10},{"isValid":false,"errors":[],"formName":"Eligibility Checklist","questionsCount":128},{"isValid":true,"errors":[],"formName":"Patient Eligibility","questionsCount":5},{"isValid":true,"errors":[],"formName":"Molecular Marker","questionsCount":4}],"checkUom":null,"checkStdCrfCde":null,"mustDisplayException":null};
    }
-   
   ngOnInit() {
   }
 
   // get usable status message to display in table status column //
-  getStatus = (isValid:boolean) => isValid ? 'Success':'Fail';
-
-  // shows or hides error row //
-  showHideError = (i:boolean) => { i['expand']=!i['expand']; return false }
+  getReadableErrorStatus = (isValid:boolean) => isValid ? 'Success':'Fail';
 
   // sorts data alphabetically by form name //
-  sortedData = () => this.data['formsList']
+  getSortedData = () => this.data['formsList']
       .sort((a, b) => a.formName.toUpperCase()<b.formName.toUpperCase() ? -1 : 1);
+
+  // shows or hides error row //
+  setExpandCollapse = (i:boolean) => { i['expand']=!i['expand']; return false }      
 };
