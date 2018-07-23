88d8d3cbc334b78f5583d8c1474e5b8988cac3e9
regulator95 <seinolf@gmail.com>
2018-07-23 16:09:34 -0400

    added git pull to run.sh

--- docker/cchecker-ui-docker/run.sh
+++ docker/cchecker-ui-docker/run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 if [ -d "/root/cadsr-services" ]; then
-	echo "Skipping cc ui install"
+        echo "Skipping cc ui install"
+  cd /root/cadsr-services
+  git pull
 else
   cd /root
   git clone https://github.com/CBIIT/cadsr-services
 fi
 cd /root/cadsr-services/cchecker-ui
-npm install 
+npm install
 ng build --output-path /var/www/localhost/htdocs --watch &
 
 exec httpd -D FOREGROUND
