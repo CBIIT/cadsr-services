3b25f744cdffc7cd23808490ac17cf250bf16142
regulator95 <seinolf@gmail.com>
2018-07-24 16:14:41 -0400

    removed form component

--- cchecker-ui/src/app/app.module.ts
+++ cchecker-ui/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {Routes,  RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
-import { AlsFormComponent } from './als-form/als-form.component';
 import { AlsFormListComponent } from './als-form-list/als-form-list.component';
 import { AlsUploadFormComponent } from './als-upload-form/als-upload-form.component';
 import { AlsReportComponent } from './als-report/als-report.component';
@@ -19,7 +18,6 @@ const APP_ROUTES: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    AlsFormComponent,
     AlsFormListComponent,
     AlsUploadFormComponent,
     AlsReportComponent,
