efe88792ded4d3c9819a0290db9473e38e07d288
vikramsan <vikram.san@gmail.com>
2018-07-23 21:36:04 -0400

    Fix for errors not being returned in the form list to UI

--- cchecker-gateway/src/main/java/gov/nih/nci/cadsr/service/FormService.java
+++ cchecker-gateway/src/main/java/gov/nih/nci/cadsr/service/FormService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import gov.nih.nci.cadsr.data.ALSData;
+import gov.nih.nci.cadsr.data.ALSError;
 import gov.nih.nci.cadsr.data.ALSForm;
 import gov.nih.nci.cadsr.data.FormDisplay;
 import gov.nih.nci.cadsr.data.FormsUiData;
@@ -16,6 +17,7 @@ import gov.nih.nci.cadsr.data.FormsUiData;
 public class FormService {
 
 	private static final Logger logger = Logger.getLogger(FormService.class);
+	private static String errorSeverity_warn = "WARNING";
 	
 	public static FormsUiData buildFormsUiData (ALSData alsData) {
 		List<FormDisplay> formsList = new ArrayList<FormDisplay>();
@@ -24,6 +26,13 @@ public class FormService {
 				FormDisplay fd = new FormDisplay();
 				fd.setFormName(form.getDraftFormName());
 				fd.setIsValid(true);
+				for (ALSError error : alsData.getCccError().getAlsErrors()) {
+					if (error.getFormOid()!=null && error.getFormOid().equalsIgnoreCase(form.getFormOId()) && !error.getErrorSeverity().equals(errorSeverity_warn) ) {
+						fd.getErrors().add(error);
+					}
+				}
+				if (!fd.getErrors().isEmpty())
+					fd.setIsValid(false);					
 				int qCount = form.getFields().size();
 				fd.setQuestionsCount(qCount);
 				formsList.add(fd);
