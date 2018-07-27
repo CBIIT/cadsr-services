e46ebe10cf9ff2bc9bddd13fa6ea226fd7243a61
regulator95 <seinolf@gmail.com>
2018-07-27 18:28:14 -0400

    expose port 1551 and add service-db mvn build

--- docker/cchecker-server-docker/entrypoint/run.sh
+++ docker/cchecker-server-docker/entrypoint/run.sh
@@ -1,9 +1,20 @@
 #~/bin/sh
+
+# pull up to date code #
 cd cadsr-services
-git pull
+git pull 
+
+# buld and run the cchecker parser #
 cd cchecker-service-parser
 mvn package
 java -jar target/cchecker-service-parser-0.0.1-SNAPSHOT.jar &
+
+# build and run the cchecker db service #
+cd ../cchecker-service-db
+mvn clean package
+java -jar target/cchecker-service-db-0.0.1-SNAPSHOT.jar &
+
+# build and run the cchecker gateway #
 cd ../cchecker-gateway
 mvn package
 cp target/cchecker-gateway-0.0.1-SNAPSHOT.war ./gateway.war
