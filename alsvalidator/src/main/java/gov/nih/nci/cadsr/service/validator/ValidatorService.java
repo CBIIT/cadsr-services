2f25e9b80eb918b04af91db15792a1977eb0b59f
vikramsan <vikram.san@gmail.com>
2018-07-27 17:58:20 -0400

    Adding data formats to compare the ALS file & caDSR DB

--- alsvalidator/src/main/java/gov/nih/nci/cadsr/service/validator/ValidatorService.java
+++ alsvalidator/src/main/java/gov/nih/nci/cadsr/service/validator/ValidatorService.java
@@ -18,28 +18,30 @@ import gov.nih.nci.cadsr.service.model.cdeData.dataElement.ReferenceDocument;
 
 public class ValidatorService {
 
-	private static final Logger logger = Logger.getLogger(ValidatorService.class);	
+	private static final Logger logger = Logger.getLogger(ValidatorService.class);
 	private static final String errorString = "ERROR";
 	private static final String matchString = "MATCH";
-	private static final String warningString = "WARNING";	
+	private static final String warningString = "WARNING";
 	private static final String retiredString = "RETIRED";
 	private static final String msg1 = "CDE not in caDSR database";
 	private static final String msg2 = "CDE has been retired";
 	private static final String msg3 = "Newer Versions exist";
-	private static String congStatus_errors= "ERRORS";
+	private static String congStatus_errors = "ERRORS";
 	private static String congStatus_warn = "WARNINGS";
-	private static String congStatus_congruent = "CONGRUENT";	
-	private static List<String> characterDataFormats = Arrays.asList("CHAR", "VARCHAR2", "CHARACTER");
-	private static List<String> numericDataFormats = Arrays.asList("NUMBER", "number", "numeric", "integer");	
+	private static String congStatus_congruent = "CONGRUENT";
+	private static List<String> characterDataFormats = Arrays.asList("CHAR", "VARCHAR2", "CHARACTER", "ALPHANUMERIC", "java.lang.String", "java.lang.Character", 
+			"xsd:string");
+	private static List<String> numericDataFormats = Arrays.asList("NUMBER", "number", "numeric", "integer", "Integer", "java.lang.Integer", "xsd:integer");
+	private static List<String> dateDataFormats = Arrays.asList("DATE", "xsd:date");
+	private static List<String> timeDataFormats = Arrays.asList("TIME", "xsd:time");
 	private static final String characters_string = "characters";
 	private static final String punct_pattern = "\\p{P}";
-	private static final String patternHolderChar = "d";	
-	
-	
+	private static final String patternHolderChar = "d";
+
 	public static CCCQuestion validate(ALSField field, CCCQuestion question, CdeDetails cdeDetails) {
 		StringBuffer message = new StringBuffer();
 		try {
-		if (cdeDetails == null) {
+		if (cdeDetails.getDataElement()==null && cdeDetails.getValueDomain()==null) {
 			message.append(msg1);
 			question.setQuestionCongruencyStatus(congStatus_errors);
 			logger.debug("CDE data not available for "+question.getCdePublicId());
@@ -67,20 +69,18 @@ public class ValidatorService {
 				vdMaxLen = cdeDetails.getValueDomain().getValueDomainDetails().getMaximumLength();
 				for (PermissibleValuesModel pv : cdeDetails.getValueDomain().getPermissibleValues()) {
 					pvList.add(pv.getValue());
+					pvVmList.add(pv.getShortMeaning());
 					if (allowableCdes.length() > 0)
 						allowableCdes.append("|"+pv.getValue());
 					else 
 						allowableCdes.append(pv.getValue());
 					if (pv.getValue().length() > pvMaxLen)
 						pvMaxLen = pv.getValue().length();
-					
-					pvVmList.add(pv.getShortMeaning());
 				}
 			}			
 			
 			// Checking for the presence of RAVE user data string in the PV Value meaning list - PV Checker result
-			question = setPvCheckerResult (pvVmList, question);
-			
+			question = setPvCheckerResult (pvVmList, question);			
 			
 			// Setting the Allowable CDEs
 			if (allowableCdes.length() > 0) 
@@ -225,22 +225,27 @@ public class ValidatorService {
 
 	protected static CCCQuestion checkDataTypeCheckerResult (CCCQuestion question, String raveDataFormat, String vdDataType) {
 		// Comparing RAVE Data format with caDSR Value Domain Datatype - Datatype Checker Result
+		Boolean result = false;
 		if (raveDataFormat!=null) {
-			if (raveDataFormat.indexOf("$") > -1) {
-				if (characterDataFormats.contains(vdDataType))  {
-					question.setDatatypeCheckerResult(matchString);
-				} else {
-					question.setDatatypeCheckerResult(errorString);
-					question.setQuestionCongruencyStatus(congStatus_errors);
-				} 
+			if (raveDataFormat.startsWith("$")) {
+				if (characterDataFormats.contains(vdDataType)) 
+					result = true;
+			} else if (raveDataFormat.startsWith("dd") || raveDataFormat.startsWith("DD")) {
+				if (dateDataFormats.contains(vdDataType)) 
+					result = true;
+			} else if (raveDataFormat.startsWith("hh") || raveDataFormat.startsWith("HH")) {
+				if (timeDataFormats.contains(vdDataType))
+					result = true;
 			} else {
-				if (numericDataFormats.contains(vdDataType))  {
-					question.setDatatypeCheckerResult(matchString);
-				} else {
-					question.setDatatypeCheckerResult(errorString);
-					question.setQuestionCongruencyStatus(congStatus_errors);
-				}
+				if (numericDataFormats.contains(vdDataType))
+					result = true;
 			}
+		}
+		if (result)
+			question.setDatatypeCheckerResult(matchString);
+		else {
+			question.setDatatypeCheckerResult(errorString);
+			question.setQuestionCongruencyStatus(congStatus_errors);
 		}
 		return question;
 	}
@@ -301,5 +306,5 @@ public class ValidatorService {
 		}
 		return question;
 	}
-	
+
 }
