4926dab06946bf6b82b91209f3f285208c963e77
regulator95 <seinolf@gmail.com>
2018-07-28 12:37:28 -0400

    added docker-compose for easier installation.

--- docker/cchecker-server-docker/entrypoint/run.sh
+++ docker/cchecker-server-docker/entrypoint/run.sh
@@ -1,23 +1,38 @@
 #~/bin/sh
 
 # pull up to date code #
+echo "entering cadsr-services directory"
 cd cadsr-services
+echo "pulling latest code from github"
 git pull 
 
 # buld and run the cchecker parser #
+echo "entering cchecker-service-parser directory"
 cd cchecker-service-parser
+echo "building cchecker-service-parser"
 mvn package
 java -jar target/cchecker-service-parser-0.0.1-SNAPSHOT.jar &
+echo "done building cchecker-service-parser"
 
 # build and run the cchecker db service #
+echo "entering cchecker-service-db directory"
 cd ../cchecker-service-db
+echo "building cchecjer-service-db"
 mvn clean package
 java -jar target/cchecker-service-db-0.0.1-SNAPSHOT.jar &
+echo "done building cchecker-service-db" 
 
 # build and run the cchecker gateway #
+echo "entering cchecker-gateway directory"
 cd ../cchecker-gateway
+echo "building cchecker-gateway"
 mvn package
 cp target/cchecker-gateway-0.0.1-SNAPSHOT.war ./gateway.war
 mv gateway.war /usr/local/apache-tomcat/webapps
+echo "done building cchecker-gateway"
+
+echo "entering apache-tomcat directory"
+echo "starting tomcat"
 cd /usr/local/apache-tomcat
 ./bin/catalina.sh run
+echo "done starting tomcat"
--- docker/cchecker-ui-docker/README.md
+++ docker/cchecker-ui-docker/README.md
@@ -1,4 +1,4 @@
-docker configuration for cchecker-gateway
+docker configuration for cchecker-ui
 =====
 
 ## Requirements ##
@@ -6,14 +6,15 @@ docker configuration for cchecker-gateway
 
 ## Installation ##
 1. git clone https://github.com/CBIIT/cadsr-services
-2. cd cadsr-services/docker/cchecker-server-docker
-3. run ./build.sh
-4. When tomcat starts and window output stops tomcat is started
+2. cd cadsr-services/docker/cchecker-ui-docker
+3. run ./build.sh cchecker-ui 80
+4. give container a minute or two to completely start angular ui and apache
+5. visit http://localhost in a browser
 
 ## To start docker container once installed ##
 * This only needs to be done if the container is already installed and has previously been stopped
 
-1. docker start -a cchecker-gateway
+1. docker start -a cchecker-ui
 
 ## To stop docker container once installed ##
-1. docker stop cchecker-gateway
+1. docker stop cchecker-ui
