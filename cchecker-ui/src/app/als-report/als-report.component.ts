4d6e70a87bb6fa0a47eb328d686fe5964b682f79
regulator95 <seinolf@gmail.com>
2018-08-09 16:45:04 -0400

    modified report page to use datatables

--- cchecker-ui/src/app/als-report/als-report.component.ts
+++ cchecker-ui/src/app/als-report/als-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from '../../../node_modules/rxjs';
 import { ReportService } from '../services/report.service';
+import { RouterLinkWithHref } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-als-report',
@@ -9,11 +10,35 @@ import { ReportService } from '../services/report.service';
 })
 export class AlsReportComponent implements OnInit {
   private reportData:Observable<Object>;
-
+  private dtOptions:Object;
   constructor(private reportService:ReportService) { }
 
   ngOnInit() {
     this.reportData = this.reportService.getReportData();
+    this.dtOptions={
+      columns: [
+        {
+          width:"50%",
+          cellType:"th"
+        },
+        {
+          width:"50%",
+          cellType:"th"
+        },
+      ],
+      rowCallback: (r,d,e) => {
+        $('td', r).unbind('click');
+        $('td', r).bind('click', () => {
+          console.log("A")
+        });
+        return r;
+      },
+
+      ordering:false,
+      paging:false,
+      searching:false,
+      info:false
+    };
   }
 
 }
