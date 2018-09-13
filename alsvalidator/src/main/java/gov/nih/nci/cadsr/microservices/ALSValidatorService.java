dd620658452793e1136f6fedeb02dbd1162715a3
asafieva <natalia.asafieva@nih.gov>
2018-09-13 17:53:30 -0400

    Removed duplication calls to CDE Browser RESTful API

--- alsvalidator/src/main/java/gov/nih/nci/cadsr/microservices/ALSValidatorService.java
+++ alsvalidator/src/main/java/gov/nih/nci/cadsr/microservices/ALSValidatorService.java
@@ -6,31 +6,16 @@ package gov.nih.nci.cadsr.microservices;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.nih.nci.cadsr.data.CCCQuestion;
-import gov.nih.nci.cadsr.data.CCCReport;
-import gov.nih.nci.cadsr.data.ReportInputWrapper;
-import gov.nih.nci.cadsr.data.ValidateDataWrapper;
-import gov.nih.nci.cadsr.report.ReportOutput;
-import gov.nih.nci.cadsr.report.impl.GenerateReport;
-import gov.nih.nci.cadsr.service.validator.ValidatorService;
+@SpringBootApplication(scanBasePackages = { "gov.nih.nci.cadsr.microservices"})
 
-@SpringBootApplication(scanBasePackages = { "gov.nih.nci.cadsr.microservices" })
 @RestController
 @EnableAutoConfiguration
 // (exclude={DataSourceAutoConfiguration.class,
@@ -38,6 +23,12 @@ import gov.nih.nci.cadsr.service.validator.ValidatorService;
 public class ALSValidatorService {
 	private static final Logger logger = LoggerFactory.getLogger(ALSValidatorService.class);
 	protected static String CCHECKER_DB_SERVICE_URL_RETRIEVE;
+	protected static String CDEBROWSER_REST_GET_CDE;
+	
+	public static String getCDEBROWSER_REST_GET_CDE() {
+		return CDEBROWSER_REST_GET_CDE;
+	}
+
 	@RequestMapping("/")
 	String home() {
 		return "ALS Congruency Checker Validator Service is running!\n";
@@ -51,6 +42,8 @@ public class ALSValidatorService {
 		properties.load(input);
 		CCHECKER_DB_SERVICE_URL_RETRIEVE = properties.getProperty("CCHECKER_DB_SERVICE_URL_RETRIEVE");
 		logger.info("CCHECKER_DB_SERVICE_URL_RETRIEVE: " + CCHECKER_DB_SERVICE_URL_RETRIEVE);
+		CDEBROWSER_REST_GET_CDE = properties.getProperty("CDEBROWSER_REST_GET_CDE");
+		logger.info("CDEBROWSER_REST_GET_CDE: " + CDEBROWSER_REST_GET_CDE);
 		
 		SpringApplication.run(ALSValidatorService.class, args);
 	}
