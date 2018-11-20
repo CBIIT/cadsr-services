2cc8cb9a0d35fd1b39d065da8046489b32f08a70
asafieva <natalia.asafieva@nih.gov>
2018-11-20 13:22:51 -0500

    removed tested which used a removed method

--- alsvalidator/src/test/java/gov/nih/nci/cadsr/service/validator/ValidatorServiceTest.java
+++ alsvalidator/src/test/java/gov/nih/nci/cadsr/service/validator/ValidatorServiceTest.java
@@ -6,32 +6,4 @@ import org.junit.Test;
 
 public class ValidatorServiceTest {
 
-	@Test
-	public void testCleanUpNonUtf8Same() {
-		String expected = "stringToClean";
-		String received = ValidatorService.cleanUpNonUtf8(expected);
-		assertEquals(expected, received);
-	}
-	@Test
-	public void testCleanUpNonUtf8Diamond() {
-		String expected = "-" + "�";
-		String received = ValidatorService.cleanUpNonUtf8(expected);
-//		System.out.println("...testCleanUpNonUtf8Diamond\nreceived: " + received + "\nexpected: " + expected + 
-//			"\nlength received: " + received.length() + "\nlength expected: " + expected.length());
-	}
-	@Test
-	public void testCleanUpNonUtf8B2() {
-		String expected = "stringToClean" + ValidatorService.apostrophe_str;
-		String received = ValidatorService.cleanUpNonUtf8(expected);
-//		System.out.println("...testCleanUpNonUtf8B2\nreceived: " + received + "\nexpected: " + expected + 
-//			"\nlength received: " + received.length() + "\nlength expected: " + expected.length());
-
-	}
-	@Test
-	public void testCleanUpNonUtf8BF() {
-		String expected = "stringToClean" + ValidatorService.superscript_2_str;
-		String received = ValidatorService.cleanUpNonUtf8(expected);
-//		System.out.println("...testCleanUpNonUtf8BF\nreceived: " + received + "\nexpected: " + expected+ 
-//			"\nlength received: " + received.length() + "\nlength expected: " + expected.length());
-	}
 }
