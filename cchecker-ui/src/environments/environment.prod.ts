2015d84eff1f8322fb1ffb295eca34a77b5dc035
Shaun Einolf <seinolf@gmail.com>
2018-12-14 09:50:20 -0500

    Update environment.prod.ts

--- cchecker-ui/src/environments/environment.prod.ts
+++ cchecker-ui/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl:'REPLACEME',
+  timestamp: '',
   cdeBrowserUrl:'https://cdebrowser.nci.nih.gov',
   formBuilderUrl:'https://formbuilder.nci.nih.gov/FormBuilder'
 };
