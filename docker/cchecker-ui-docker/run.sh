8ea0155ab3d888a3b68dcf893ce6e79cda01dde2
regulator95 <seinolf@gmail.com>
2018-12-14 10:20:58 -0500

    adding run.sh date substitute commands

--- docker/cchecker-ui-docker/run.sh
+++ docker/cchecker-ui-docker/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+timestamp = `date`
 if [ -d "/root/cadsr-services" ]; then
         echo "Skipping cc ui install"
   cd /root/cadsr-services
@@ -13,9 +14,11 @@ npm install
 
 if [ $configuration ]; then 
   sed "s#REPLACEME#$apiUrl#g" -i src/environments/environment.${configuration}.ts
+  sed "s/timestamp: ''.*/timestamp: '$timestamp',/g" -i src/environments/environment.${configuration}.ts  
   ng build -c=$configuration --output-path /var/www/html
 else
   ng build --output-path /var/www/html
+  sed "s/timestamp: ''.*/timestamp: '$timestamp',/g" -i src/environments/environment.ts
 fi
 
 cp /etc/httpd/conf.d/.htaccess /var/www/html
