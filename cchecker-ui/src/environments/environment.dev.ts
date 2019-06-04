74ddf9abee06b4e94d3d0996b19c7878c739e244
Your Name <you@example.com>
2019-06-04 10:44:07 -0400

    adding new cdash and sdtm tabs to report

--- cchecker-ui/src/environments/environment.dev.ts
+++ cchecker-ui/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   REST_API: 'http://127.0.0.1:8080',
-  timestamp: 'Tue Jun  4 10:30:44 EDT 2019',
+  timestamp: 'Tue Jun  4 10:41:57 EDT 2019',
   cdeBrowserUrl:'https://cdebrowser-dev.nci.nih.gov',
   formBuilderUrl:'https://formbuilder-dev.nci.nih.gov/FormBuilder'
 };
