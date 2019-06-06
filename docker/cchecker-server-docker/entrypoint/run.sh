c88d4e5d95f3f6b47014907aa0e99850d1f228f2
Shaun Einolf <seinolf@gmail.com>
2019-06-06 11:02:19 -0400

    Update run.sh
    changed .jar and .war file lines to remove version numbers so they are not hard coded

--- docker/cchecker-server-docker/entrypoint/run.sh
+++ docker/cchecker-server-docker/entrypoint/run.sh
@@ -30,7 +30,8 @@ echo "entering cchecker-service-db directory"
 cd ../cchecker-service-db
 echo "building cchecher-service-db"
 mvn clean package
-java -jar target/cchecker-service-db-0.0.1-SNAPSHOT.jar &
+mv target/cchecker-service-db*.jar target/cchecker-service-db.jar
+java -jar target/cchecker-service-db.jar &
 echo "done building cchecker-service-db" 
 
 # build and run the alsvalidator service #
@@ -38,7 +39,8 @@ echo "entering alsvalidator directory"
 cd ../alsvalidator
 echo "building alsvalidator"
 mvn clean package
-java -jar target/alsvalidator-0.0.1-SNAPSHOT.jar &
+mv target/alsvalidator*.jar target/alsvalidator.jar
+java -jar target/alsvalidator.jar &
 echo "done building, alsvalidator service started"
 
 # build and run the generate Excel service #
@@ -46,7 +48,8 @@ echo "entering cchecker-service-excel-gen directory"
 cd ../cchecker-service-excel-gen
 echo "building cchecker-service-excel-gen"
 mvn clean package
-java -jar target/cchecker-service-excel-gen-0.0.1-SNAPSHOT.jar &
+mv target/cchecker-service-excel-gen*.jar target/cchecker-service-excel-gen.jar
+java -jar target/cchecker-service-excel-gen.jar &
 echo "done building cchecker-service-excel-gen, cchecker-service-excel-gen service started"
 
 # build and run the cchecker gateway #
@@ -54,7 +57,8 @@ echo "entering cchecker-gateway directory"
 cd ../cchecker-gateway
 echo "building cchecker-gateway"
 mvn package
-cp target/cchecker-gateway-0.0.1-SNAPSHOT.war ./gateway.war
+mv target/cchecker-gateway*.jar target/cchecker-gateway.jar
+cp target/cchecker-gateway*.war ./gateway.war
 mv gateway.war /usr/local/apache-tomcat/webapps
 echo "done building cchecker-gateway"
 
