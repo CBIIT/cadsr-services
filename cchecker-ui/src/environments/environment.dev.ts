3ac7064a807845220f7035b1ece9d5024d216550
Shaun Einolf <seinolf@gmail.com>
2019-06-06 10:39:59 -0400

    Update environment.dev.ts

--- cchecker-ui/src/environments/environment.dev.ts
+++ cchecker-ui/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  REST_API: 'https://cdevalidator-dev.nci.nih.gov',
+  REST_API: 'REPLACEME',
   timestamp: 'Tue Jun  4 10:41:57 EDT 2019',
   cdeBrowserUrl:'https://cdebrowser-dev.nci.nih.gov',
   formBuilderUrl:'https://formbuilder-dev.nci.nih.gov/FormBuilder'
