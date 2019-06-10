712434c5b051488d16ae19dbde8ad9b686e57b7a
Shaun Einolf <seinolf@gmail.com>
2019-06-10 11:10:16 -0400

    Update run.sh
    adding comment

--- docker/cchecker-server-docker/entrypoint/run.sh
+++ docker/cchecker-server-docker/entrypoint/run.sh
@@ -79,6 +79,7 @@ while [ ! -d "/usr/local/apache-tomcat/log" ]
 done
 echo "changing permissions on apache-tomcat log directory"
 chmod 777 -R /usr/local/apache-tomcat/log
+echo "changing permissions on apache-tomcat log file"
 chmod 777 -R /usr/local/apache-tomcat/log/*
 
 echo "starting nginx web server"
