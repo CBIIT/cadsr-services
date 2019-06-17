b41f69004abea87fced9e1bc9aaecd81fcae23cf
asafieva <natalia.asafieva@nih.gov>
2019-06-17 16:15:09 -0400

    session id added to parser service log

--- cchecker-gateway/src/main/java/gov/nih/nci/cadsr/cchecker/RestCallsInterceptor.java
+++ cchecker-gateway/src/main/java/gov/nih/nci/cadsr/cchecker/RestCallsInterceptor.java
@@ -11,6 +11,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,7 @@ public class RestCallsInterceptor implements HandlerInterceptor {
 	public static final String REST_CONTROLLER_USAGE_PREFIX_RESPONSE = "CCHECKER_REST_RESPONSE";
 	public static final String REST_CONTROLLER_ERROR = "CCHECKER_REST_ERROR";
     public static final String LOG_DATE_TIME_FORMAT = "YYYY-MM-dd'T'HH:mm:ss.SSS";
-	public static final String REST_USAGE_LOG_REQUEST_FORMAT = "[%s][%s][%s][%s][%s][%s]";//prefix time method URI query cookie
+	public static final String REST_USAGE_LOG_REQUEST_FORMAT = "[%s][%s][%s][%s][%s][%s][%s]";//prefix time method URI query cookie [owner]
 	public static final String REST_USAGE_LOG_RESPONSE_FORMAT = "[%s][%s][response code %d][%s][%s][%s]";//prefix time code method URI query
 	public static final String REST_USAGE_LOG_ERROR_FORMAT = "[%s][%s][%s]";//prefix time code method URI query
 
@@ -64,7 +65,11 @@ public class RestCallsInterceptor implements HandlerInterceptor {
 				}
 			}
 		}
-		String toLog = String.format(REST_USAGE_LOG_REQUEST_FORMAT, REST_CONTROLLER_USAGE_PREFIX_REQUEST, formattedCurrentDate, request.getMethod(), request.getRequestURI(),request.getQueryString(), cookieList.toString());
+		String paramOwner = request.getParameter("owner");
+		paramOwner = (StringUtils.isNotBlank(paramOwner)) ? paramOwner : "";
+		String toLog = String.format(REST_USAGE_LOG_REQUEST_FORMAT, REST_CONTROLLER_USAGE_PREFIX_REQUEST, 
+			formattedCurrentDate, request.getMethod(), request.getRequestURI(),
+			request.getQueryString(), cookieList.toString(), paramOwner);
 		logger.info(toLog);
 		
 		return true;
