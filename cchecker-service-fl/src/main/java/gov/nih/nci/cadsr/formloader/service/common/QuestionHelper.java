f472f957c0884a21782818506ecf5359cbc55d82
asafieva <natalia.asafieva@nih.gov>
2019-07-19 10:40:25 -0400

    FORMBUILD-622 QuestionHelper FL legacy NullPointer preventing

--- cchecker-service-fl/src/main/java/gov/nih/nci/cadsr/formloader/service/common/QuestionHelper.java
+++ cchecker-service-fl/src/main/java/gov/nih/nci/cadsr/formloader/service/common/QuestionHelper.java
@@ -1,15 +1,17 @@
 package gov.nih.nci.cadsr.formloader.service.common;
 
-import gov.nih.nci.cadsr.formloader.domain.QuestionDescriptor;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gov.nih.nci.ncicb.cadsr.common.dto.DataElementTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.dto.QuestionTransferObject;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class QuestionHelper {
 
 	//JR423
 	public static final void handleEmptyQuestionText(QuestionTransferObject questdto, DataElementTransferObject cdeDto) {
+		final Logger logger = LoggerFactory.getLogger(QuestionHelper.class.getName());
 		String questionText = null;
 		//TODO to confirm "if preferred def of data element if it's not null. Otherwise, use long name of data element. if No CDE, use question text"
 		if(StringUtils.isEmpty(questdto.getPreferredDefinition()))
@@ -23,7 +25,15 @@ public class QuestionHelper {
 			}*/
 			else
 			{
-				questionText = "Data Element " + cdeDto + " does not have Preferred Question Text";	//TODO test it and confirm that if is it not cdeDto.getLongName()
+				//FORMBUILD-622 we prevent null pointer on cdeDto for error cases cdeDto is null.
+				String longText;
+				if (cdeDto == null) {
+					logger.warn("QuestionHelper.handleEmptyQuestionText received empty cdeDto for questdto: " + questdto);
+					longText = "null";
+				}
+				else 
+					longText = cdeDto.getLongName();
+				questionText = "Data Element " + longText + " does not have Preferred Question Text";	//TODO test it and confirm that if is it not cdeDto.getLongName()
 			}
 			questdto.setLongName(questionText);
 			questdto.setPreferredDefinition(questionText);
