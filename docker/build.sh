8499cb3c46d45781ff398ee32463dad87bfca594
Shaun Einolf <seinolf@gmail.com>
2019-08-28 10:01:10 -0400

    Update build.sh
    adding no-cache to build to force rebuild

--- docker/build.sh
+++ docker/build.sh
@@ -2,13 +2,13 @@
 TAG=`date +%F`-`git log -1 --pretty=%h`
 
 echo "Building ui image"
-docker build ./cchecker-ui-docker -t temp_ui
+docker build --no-cache ./cchecker-ui-docker -t temp_ui
 echo "tagging ui image to date/time-hash tag and latest tag"
 docker tag temp_ui:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:$TAG
 docker tag temp_ui:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:latest
 
 echo "Building server image"
-docker build ./cchecker-server-docker -t temp_server
+docker build --no-cache ./cchecker-server-docker -t temp_server
 echo "tagging server image to date/time-hash tag and latest tag"
 docker tag temp_server:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:$TAG
 docker tag temp_server:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:latest
