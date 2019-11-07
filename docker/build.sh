723b26d99eef58a695056c4bfc2cfdf63f3d7069
Shaun Einolf <seinolf@gmail.com>
2019-11-07 12:16:01 -0500

    Update build.sh
    modifying build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-TAG=`date +%F`-`git log -1 --pretty=%h`
 
 echo "Building ui image"
-docker build --no-cache ./cchecker-ui-docker -t temp_ui
-echo "tagging ui image to date/time-hash tag and latest tag"
-docker tag temp_ui:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:$TAG
-docker tag temp_ui:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:latest
+docker build --no-cache ./cchecker-ui-docker -t ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:$TAG
+docker tag ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:$TAG ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:latest
 
 echo "Building server image"
-docker build --no-cache ./cchecker-server-docker -t temp_server
-echo "tagging server image to date/time-hash tag and latest tag"
-docker tag temp_server:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:$TAG
-docker tag temp_server:latest ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:latest
+docker build --no-cache ./cchecker-server-docker -t ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:$TAG
+docker tag ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:$TAG ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:latest
 
 echo "pushing images to repository"
 docker push ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:latest
@@ -19,6 +14,4 @@ docker push ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-ui:$TAG
 docker push ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:$TAG
 docker push ncidockerhub.nci.nih.gov/cadsrdocker/cchecker-server:latest
 
-echo "removing unused images from system"
-docker rmi temp_ui:latest
-docker rmi temp_server:latest
+docker image prune -f
