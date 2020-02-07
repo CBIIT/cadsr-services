a34711f8e0b62be2554d62c3ad37b02f4a6ed389
asafieva <natalia.asafieva@nih.gov>
2020-02-07 16:43:25 -0500

    Removing dependency jackson-mapper-asl. Springboot update to 2.2.4

--- cchecker-service-db/src/main/java/gov/nih/nci/cadsr/microservices/DataElementRepository.java
+++ cchecker-service-db/src/main/java/gov/nih/nci/cadsr/microservices/DataElementRepository.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
